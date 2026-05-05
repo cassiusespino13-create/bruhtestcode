bf98e775bea8ed11ac4676e664563a1bbcca2c53
cassiusespino13-create <cassius.espino13@gmail.com>
2026-05-05 19:33:39 +0800

    v2.1.1
    Changes:
    added slime enemie opps

--- Scripts/enemy_slime.gd
+++ Scripts/enemy_slime.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+const SPEED = 30
+
+var direction = 1
+
+@onready var raycastright: RayCast2D = $Raycastright
+@onready var raycastleft: RayCast2D = $Raycastleft
+
 
 func _process(delta: float) -> void:
-	pass
+	if raycastright.is_colliding():
+		direction = -1
+	if raycastleft.is_colliding():
+		direction = 1
+	
+	position.x += direction * SPEED * delta
